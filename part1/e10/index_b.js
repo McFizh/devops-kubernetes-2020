ba42021bd386af99d9d5b4a3d8a89ad9d421ebe8
Pekka Harjamäki <spamtrap_github@mcfish.org>
2020-11-02 21:14:14 +0200

    Added part1 exercise 11

--- part1/e10/index_b.js
+++ part1/e10/index_b.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const Uuid = require('uuid');
+const express = require('express');
+const app = express();
+require('dotenv').config();
 
+const port = process.env.PORT || 3000;
 const str = Uuid.v4();
+
 let prevStamp = '';
+let pongCount = '';
 
 const readStamp = () => {
+
   if(fs.existsSync('/shared/timestamp.txt')) {
     fs.readFile('/shared/timestamp.txt', 'ascii', (err, data) => {
       if(data !== prevStamp) {
@@ -13,10 +20,28 @@ const readStamp = () => {
       }
     })
   }
+
+  if(fs.existsSync('/shared/pongcount.txt')) {
+    fs.readFile('/shared/pongcount.txt', 'ascii', (err, data) => {
+      pongCount = data;
+    })
+  }
+
 }
 
-setInterval( () => {
-  readStamp();
-}, 2000);
 
-readStamp();
+app.get('/', (req, res) => {
+  const page = `
+${prevStamp} ${str}
+Ping / Pongs: ${pongCount}
+`;
+  res.send(page);
+});
+
+app.listen(port, () => {
+  readStamp();
+  setInterval( () => {
+    readStamp();
+  }, 2000);
+});
+
--- part1/e9/index.js
+++ part1/e9/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 require('dotenv').config();
@@ -5,9 +6,18 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 let count = 0;
 
+const writePongCount = () => {
+  if(fs.existsSync('/shared')) {
+    fs.writeFile('/shared/pongcount.txt', `${count}`, () => {});
+  }
+};
+
 app.get('/', (req, res) => {
   res.send(`pong ${count}`);
   count++;
+  writePongCount();
 });
 
-app.listen(port);
+app.listen(port, () => {
+  writePongCount();
+});
