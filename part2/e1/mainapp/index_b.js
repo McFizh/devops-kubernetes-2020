68f64d01e84892eb47a289ee6c52375983047f38
Pekka Harjamäki <spamtrap_github@mcfish.org>
2020-11-04 22:27:45 +0200

    Added part 2 exercise 1

---
+++ part2/e1/mainapp/index_b.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const Uuid = require('uuid');
+const express = require('express');
+const Got = require('got');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const str = Uuid.v4();
+
+let prevStamp = '';
+
+const readStamp = () => {
+
+  if(fs.existsSync('/shared/timestamp.txt')) {
+    fs.readFile('/shared/timestamp.txt', 'ascii', (err, data) => {
+      if(data !== prevStamp) {
+        console.log(`${data} ${str}`);
+        prevStamp = data;
+      }
+    })
+  }
+
+}
+
+
+app.get('/', async (req, res) => {
+
+  const { body: rspBody } = await Got('http://pongapp-service:2345/count');
+  const { count } = JSON.parse(rspBody);
+
+  const page = `
+${prevStamp} ${str}
+Ping / Pongs: ${count}
+`;
+  res.send(page);
+});
+
+app.listen(port, () => {
+  readStamp();
+  setInterval( () => {
+    readStamp();
+  }, 2000);
+});
+
