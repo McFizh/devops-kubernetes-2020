4d4d408a65719e340ab39fcbbd1f17cbe4e37587
Pekka Harjamäki <spamtrap_github@mcfish.org>
2020-11-18 22:15:00 +0200

    Part3 - Ex 1

--- part2/e7/mainapp/index_b.js
+++ part2/e7/mainapp/index_b.js
@@ -5,6 +5,8 @@ const pg = require('pg');
 
 require('dotenv').config();
 
+const { initDb, getClient } = require('./database');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const str = Uuid.v4();
@@ -12,51 +14,10 @@ const str = Uuid.v4();
 let prevStamp = '';
 
 // Setup database
-const pgpwd = process.env.POSTGRES_PASSWORD;
-const pghost = process.env.POSTGRES_HOST;
-let client = null;
+initDb();
 
-const setupDb = async () => {
-  client = new pg.Client({
-    user: 'postgres',
-    host: pghost || 'localhost',
-    password: pgpwd,
-    database: 'postgres'
-  });
-
-  try {
-    await client.connect();
-    return 1;
-  } catch(err) {
-    if(err.code === 'ECONNREFUSED') {
-      client.end();
-      delete client;
-      return 0;
-    }
-    return -1;
-  }
-}
-
-if(pgpwd) {
-  (async() => {
-    if(await setupDb() === 0) {
-      console.log('Unable to connect to db.. Reattempting connection in 10s..')
-      setTimeout(async () => {
-        console.log('Attempting to reconnect')
-        const rsp = await setupDb();
-        if(rsp !== 1) {
-          console.log('Unable to connect to db');
-          process.exit(-1);
-        }
-        console.log('Connected..')
-      }, 10000);
-    }
-  })();
-}
-
-
+//
 const readStamp = () => {
-
   if(fs.existsSync('/shared/timestamp.txt')) {
     fs.readFile('/shared/timestamp.txt', 'ascii', (err, data) => {
       if(data !== prevStamp) {
@@ -65,17 +26,20 @@ const readStamp = () => {
       }
     })
   }
-
 }
-
 
 app.get('/', async (req, res) => {
   let count = 0;
 
-  if(pgpwd) {
-    const rsp = await client.query(`select value from appvalues where name='count'`);
-    if(rsp.rows.length === 1) {
-      count = rsp.rows[0].value;
+  const client = getClient();
+  if(client) {
+    try {
+      const rsp = await client.query(`select value from appvalues where name='count'`);
+      if(rsp.rows.length === 1) {
+        count = rsp.rows[0].value;
+      }  
+    } catch(err) {
+      console.log(err);
     }
   }
 
@@ -96,6 +60,8 @@ ${page}
 });
 
 app.listen(port, () => {
+  console.log(`Main app (B) listening on ${port}`);
+
   readStamp();
   setInterval( () => {
     readStamp();
--- part2/e7/pingpong/index.js
+++ part2/e7/pingpong/index.js
@@ -5,68 +5,25 @@ const pg = require('pg');
 
 require('dotenv').config();
 
+const { initDb, getClient } = require('./database');
+
 // Setup database
-const pgpwd = process.env.POSTGRES_PASSWORD;
-const pghost = process.env.POSTGRES_HOST;
-let client = null;
-
-const setupDb = async () => {
-  client = new pg.Client({
-    user: 'postgres',
-    host: pghost || 'localhost',
-    password: pgpwd,
-    database: 'postgres'
-  });
-
-  try {
-    await client.connect();
-    await client.query('create table IF NOT EXISTS appvalues (name varchar, value int);');
-    await client.query('truncate appvalues;');
-    await client.query('insert into appvalues (name, value) values (\'count\',0);');
-    return 1;
-  } catch(err) {
-    if(err.code === 'ECONNREFUSED') {
-      client.end();
-      delete client;
-      return 0;
-    }
-    console.log(err);
-    return -1;
-  }
-}
-
-if(pgpwd) {
-  (async() => {
-    const rspCode = await setupDb();
-    if(rspCode === 0) {
-      console.log('Unable to connect to db.. Reattempting connection in 10s..')
-      setTimeout(async () => {
-        console.log('Attempting to reconnect');
-        const rsp = await setupDb();
-        if(rsp !== 1) {
-          console.log('Unable to connect to db');
-          process.exit(-1);
-        }
-        console.log('Connected..');
-      }, 10000);
-    } else if(rspCode === 1) {
-      console.log('Connected.');
-    }
-  })();
-}
+initDb();
 
 //
 const port = process.env.PORT || 3000;
 let count = 0;
 
 app.get('/', async (req, res) => {
-  if(pgpwd) {
+  const client = getClient();
+  if(client) {
     try {
       await client.query(`update appvalues set value=${count} where name='count'`);
     } catch(err) {
       console.log(err);
     }
   }
+
   res.send(`pong ${count}\n`);
   count++;
 });
@@ -76,5 +33,5 @@ app.get('/count', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Pong app listening on ${app.port}`);
+  console.log(`Pong app listening on ${port}`);
 });
